7a2d7294220b4c3831d78ec53e13f121f0618a6c
antonio-fiorentino <antoniofiorentino16@gmail.com>
2021-01-08 17:47:05 +0100

    Esercizio

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 function randomNumber(min, max) {
   var result = Math.floor(Math.random() * (max + 1 - min) + min);
   return result;
-
 }
 
 var numeri = [];
 
 for (var i = 1; i <= 16; i++) {
-  var random = console.log(randomNumber(1, 100));
+  var random = randomNumber(1, 100);
+  console.log(random);
   numeri.push(random);
 }
