1c08c611f3fef391be8855a6101cafefc62c0e4d
antonio-fiorentino <antoniofiorentino16@gmail.com>
2021-01-11 16:41:15 +0100

    Esercizio

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var numeriPc = [];
 var numeriUtente = [];
 
 while (numeriPc.length<4) {
-  var random = randomNumber(1, 20);
+  var random = randomNumber(1, 10);
   console.log(random);
   if (numeriPc.includes(random)=== false) {
     numeriPc.push(random);
@@ -17,28 +17,44 @@ while (numeriPc.length<4) {
 }
 console.log(numeriPc);
 
-while (numeriUtente.length<16) {
+while (numeriUtente.length<8) {
   var sceltaUtente = parseInt(prompt('Inserisci un numero da 1 a 100'));
-  switch (sceltaUtente) {
-    case sceltaUtente > 100 || sceltaUtente < 1:
-      alert('Solo numeri compresi tra 1 e 100');
-      break;
-    case numeriPc.includes(sceltaUtente)=== true:
-      alert('Mi dispiace, hai perso');
-      break;
-    case numeriUtente.includes(sceltaUtente)=== false:
-      numeriUtente.push(sceltaUtente);
-      break;
-    case numeriUtente.includes(sceltaUtente)=== true:
-      alert('Non puoi inserire lo stesso numero');
-      break;
-    case numeriUtente === 16:
-      alert('Complimenti hai vinto');
-      break;
-    default:
-
+  if (numeriUtente.includes(sceltaUtente)=== false) {
+    numeriUtente.push(sceltaUtente);
+  }else if (numeriUtente.includes(sceltaUtente)=== true) {
+    alert('Non puoi inserire lo stesso numero');
+  }else if (sceltaUtente > 100 || sceltaUtente < 1) {
+    alert('Solo numeri compresi tra 1 e 100');
+  }else if (numeriPc.includes(sceltaUtente)=== true) {
+    alert('Mi dispiace, hai perso');
+  }else if (numeriUtente === 16) {
+    alert('Complimenti hai vinto');
   }
 }
+console.log(numeriUtente);
+
+// while (numeriUtente.length<16) {
+//   var sceltaUtente = parseInt(prompt('Inserisci un numero da 1 a 100'));
+//   switch (sceltaUtente) {
+//     case numeriUtente.includes(sceltaUtente)=== false:
+//       numeriUtente.push(sceltaUtente);
+//       break;
+//     case numeriUtente.includes(sceltaUtente)=== true:
+//       alert('Non puoi inserire lo stesso numero');
+//       break;
+//     case sceltaUtente > 100 || sceltaUtente < 1:
+//       alert('Solo numeri compresi tra 1 e 100');
+//       break;
+//     case numeriPc.includes(sceltaUtente)=== true:
+//       alert('Mi dispiace, hai perso');
+//       break;
+//     case numeriUtente === 16:
+//       alert('Complimenti hai vinto');
+//       break;
+//     default:
+//
+//   }
+// }
 
 // while (numeriUtente.length<4) {
 //   var sceltaUtente = prompt('Scegli un numero')
